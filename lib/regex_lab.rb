5ba3fb353465a1e6b13c60b01f9ea818eae60e52
Evan Burton <evan@heyevan.com>
2020-04-01 23:55:10 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,21 @@
 def starts_with_a_vowel?(word)
 
+if word.match(/\A[AEIOU]/i)
+return true
+else
+  return false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
 
-end
+text.scan(/\bun\w+ing\b/)
+
+  end
+
 
 def words_five_letters_long(text)
-
+text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
@@ -15,5 +23,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-
-end
+phone.match(/\d\d\d.\d\d\d.\d\d\d\d/)
+  end
