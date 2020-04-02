f2293dc8deee0242400ec8feb704b986f0d45cd9
Evan Burton <evan@heyevan.com>
2020-04-02 03:47:53 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -29,5 +29,9 @@ end
 
 
 def valid_phone_number?(phone)
-phone.match(/.\d{3}.\d{3}.\d{4}\b|\d{3}\d{3}\d{4}\b|\d{3}\s\d{3}\s\d{4}\b |.\d{3}.\d{3}\d{4}\b/)
+condition = phone.match(/.\d{3}.\d{3}.\d{4}\b|\d{3}\d{3}\d{4}\b|\d{3}\s\d{3}\s\d{4}\b |.\d{3}.\d{3}\d{4}\b/)
+if condition = nil
+  return false
+else
+  return true 
 end
