93392c765d081ee6cf3d98bd00b3457c885b09d8
Evan Burton <evan@heyevan.com>
2020-04-02 01:58:40 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -19,10 +19,10 @@ text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-if text.scan(/^[A-Z].$/)
+text.scan(/^[A-Z].$/)
 end
 
 def valid_phone_number?(phone)
 phone.match(/\d\d\d.\d\d\d.\d\d\d\d/)
 end
-end 
+end
