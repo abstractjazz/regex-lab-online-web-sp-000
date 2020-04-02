74fca7b50c7f82eb0bdb9e308f9524ec037be557
Evan Burton <evan@heyevan.com>
2020-04-02 03:07:04 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -19,7 +19,7 @@ text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-if text.scan(/\\b[A-Z].+\W\S\s\w.+/)
+if text.scan(/\b[A-Z].+\W\S\s\w.+/)
   return true
 else
   return false
