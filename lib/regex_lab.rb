511e39aa750ae0ba3aa33f8a601abda4117dce20
Evan Burton <evan@heyevan.com>
2020-04-02 02:50:29 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -19,7 +19,8 @@ text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-if text.match(/\b[A-Z].+\W/)
+if text.scan(/\b[A-Z].+\W/)
+
   return true
 else
   return false
